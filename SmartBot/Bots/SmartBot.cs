cad9e04b9ed68c9a71133eba830ae4f4c94fa7cd
Yulia Solomakha <yulichka700@gmail.com>
2024-12-04 19:25:09 +0100

    SmartBot project

---
+++ SmartBot/Bots/SmartBot.cs
@@ -0,0 +1,118 @@
+﻿using Microsoft.Bot.Builder;
+using Microsoft.Bot.Schema;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Collections.Generic;
+using SmartBots.CognitiveModels;
+
+namespace SmartBots.Bots
+{
+    public class SmartBot : ActivityHandler
+    {
+        private readonly SmartBotRecognizer _cluRecognizer;
+
+        public SmartBot(SmartBotRecognizer cluRecognizer) : base()
+        {
+            _cluRecognizer = cluRecognizer;
+        }
+
+        protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext,
+            CancellationToken cancellationToken)
+        {
+            var cluResult = await _cluRecognizer.RecognizeAsync<SmartModel>(turnContext, cancellationToken);
+            switch (cluResult.GetTopIntent().intent)
+            {
+                case SmartModel.Intent.Llamar:
+                    var llamarContact = cluResult.Entities.GetContact();
+                    string llamarText;
+
+                    if (llamarContact == null)
+                        llamarText = "No me ha sido posible detectar el contacto en tu frase";
+                    else
+                        llamarText = "Entendido, voy a llamar a " + llamarContact;
+
+                    var llamarMessage = MessageFactory.Text(llamarText, llamarText, InputHints.IgnoringInput);
+                    await turnContext.SendActivityAsync(llamarMessage, cancellationToken);
+                    break;
+
+                case SmartModel.Intent.EnviarMensaje:
+                    var mensajeContact = cluResult.Entities.GetContact();
+                    var mensajeFrase = cluResult.Entities.GetPhrase();
+                    string mensajeText;
+
+                    if (mensajeContact == null)
+                        mensajeText = "No me ha sido posible detectar el contacto en tu frase";
+                    else if (mensajeFrase == null)
+                        mensajeText = "No me ha sido posible detectar el mensaje en tu frase";
+                    else
+                        mensajeText = string.Format("Entendido, voy a enviar \"{0}\" a {1}", mensajeFrase, mensajeContact);
+
+                    var enviarMessage = MessageFactory.Text(mensajeText, mensajeText, InputHints.IgnoringInput);
+                    await turnContext.SendActivityAsync(enviarMessage, cancellationToken);
+                    break;
+
+                case SmartModel.Intent.PonerDespertador:
+                    var time = cluResult.Entities.GetTime();
+                    string timeText;
+
+                    if (time == null)
+                        timeText = "No me ha sido posible detectar el tiempo en tu frase";
+                    else
+                        timeText = "Entendido, voy a poner despertador a las" + time;
+
+                    var timeMessage = MessageFactory.Text(timeText, timeText, InputHints.IgnoringInput);
+                    await turnContext.SendActivityAsync(timeMessage, cancellationToken);
+                    break;
+
+                case SmartModel.Intent.BuscarEnInternet:
+                    var buscarFrase = cluResult.Entities.GetPhrase();
+                    string buscarText;
+
+                    if (buscarFrase == null)
+                        buscarText = "No me ha sido posible detectar el frase para buscar";
+                    else
+                        buscarText = string.Format("Entendido, voy a buscar \"{0}\" por Internet", buscarFrase);
+
+                    var buscarMessage = MessageFactory.Text(buscarText, buscarText, InputHints.IgnoringInput);
+                    await turnContext.SendActivityAsync(buscarMessage, cancellationToken);
+                    break;
+
+                case SmartModel.Intent.PonerCanción:
+                    var cancion = cluResult.Entities.GetSong();
+                    string cancionText;
+
+                    if (cancion == null)
+                        cancionText = "No me ha sido posible detectar la canción en tu frase";
+                    else 
+                        cancionText = "Entendido, voy a poner canción" + cancion;
+
+                    var cancionMessage = MessageFactory.Text(cancionText, cancionText, InputHints.IgnoringInput);
+                    await turnContext.SendActivityAsync(cancionMessage, cancellationToken);
+                    break;
+                default:
+                    // Catch all for unhandled intents
+                    var didntUnderstandMessageText = $"Perdona, no entendí lo que me pediste. Preguntame de otra forma";
+                    var didntUnderstandMessage = MessageFactory.Text(didntUnderstandMessageText, didntUnderstandMessageText, InputHints.IgnoringInput);
+                    await turnContext.SendActivityAsync(didntUnderstandMessage, cancellationToken);
+                    break;
+            }
+        }
+
+        protected override async Task OnMembersAddedAsync(IList<ChannelAccount> membersAdded, 
+            ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
+        {
+            var welcomeText = "Hola, Bienvenido a SmartBot! " +
+                "Con la ayuda de este bot, puedes llamar, escribir mensajes, configurar una alarma " +
+                "y buscar información en Internet. Sólo di el comando que quieres hacer";
+
+            foreach (var member in membersAdded)
+            {
+                if (member.Id != turnContext.Activity.Recipient.Id)
+                {
+                    await turnContext.SendActivityAsync(MessageFactory.Text(welcomeText, welcomeText), cancellationToken);
+                }
+            }
+        }
+
+    }
+}
---
+++ SmartBot/CognitiveModels/SmartModel.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.Bot.Builder;
+using Microsoft.BotBuilderSamples.Clu;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SmartBots.CognitiveModels
+{
+    public class SmartModel : IRecognizerConvert
+    {
+        public enum Intent
+        {
+            Llamar,
+            EnviarMensaje,
+            PonerDespertador,
+            BuscarEnInternet,
+            PonerCanción,
+            None
+        }
+
+        public string Text { get; set; }
+
+        public string AlteredText { get; set; }
+
+        public Dictionary<Intent, IntentScore> Intents { get; set; }
+
+        public CluEntities Entities { get; set; }
+
+        public IDictionary<string, object> Properties { get; set; }
+
+        public void Convert(dynamic result)
+        {
+            var jsonResult = JsonConvert.SerializeObject(result, 
+                new JsonSerializerSettings { NullValueHandling = NullValueHandling.Ignore });
+            var app = JsonConvert.DeserializeObject<SmartModel>(jsonResult);
+
+            Text = app.Text;
+            AlteredText = app.AlteredText;
+            Intents = app.Intents;
+            Entities = app.Entities;
+            Properties = app.Properties;
+        }
+
+        public (Intent intent, double score) GetTopIntent()
+        {
+            var maxIntent = Intent.None;
+            var max = 0.0;
+            foreach (var entry in Intents)
+            {
+                if (entry.Value.Score > max)
+                {
+                    maxIntent = entry.Key;
+                    max = entry.Value.Score.Value;
+                }
+            }
+
+            return (maxIntent, max);
+        }
+
+        public class CluEntities
+        {
+            public CluEntity[] Entities;
+
+            public CluEntity[] GetContacts() => Entities.Where(e => e.Category == "Contacto").ToArray();
+            public CluEntity[] GetTimes() => Entities.Where(e => e.Category == "Tiempo").ToArray();
+            public CluEntity[] GetPhrases() => Entities.Where(e => e.Category == "Frase").ToArray();
+            public CluEntity[] GetSongs() => Entities.Where(e => e.Category == "Canción").ToArray();
+
+            public string GetContact() => GetContacts().FirstOrDefault()?.Text;
+            public string GetTime() => GetTimes().FirstOrDefault()?.Text;
+            public string GetPhrase() => GetPhrases().FirstOrDefault()?.Text;
+            public string GetSong() => GetSongs().FirstOrDefault()?.Text;
+        }
+    }
+}
